5c28340e018f5452ffd5ca97bafe41d8df8a1a5b
LW83 <ljbwilkinson@gmail.com>
2022-11-15 15:43:10 +0000

    Add view for Profile model

--- Connector/views.py
+++ Connector/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Profile
 
-# Create your views here.
+
+class ProfileList(generic.ListView):
+    model = Profile
+    queryset = Profile.objects.filter(status=0).order_by('-urgency', 'profile_added')
+    template_name = 'index.html'
+    paginate_by = 50
