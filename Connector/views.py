a5587e6130a5bce3cc3559cd7ab9cfbffee378c1
LW83 <ljbwilkinson@gmail.com>
2022-11-18 16:52:46 +0000

    Update Profile List view to include status 1 and 0 objects

--- Connector/views.py
+++ Connector/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Profile
 
 
 class ProfileList(generic.ListView):
     model = Profile
-    queryset = Profile.objects.filter(status=0).order_by('-urgency', 'profile_added')
+    queryset = Profile.objects.filter(status=0) | Profile.objects.filter(status=1)
     template_name = 'index.html'
     paginate_by = 50
+
+
