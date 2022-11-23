1cbe07d3949261f0dbdab8fc7632093629b61bf2
LW83 <ljbwilkinson@gmail.com>
2022-11-23 18:23:49 +0000

    Django auth installed. Index.html updated to display table

--- PP4/urls.py
+++ PP4/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Connector.urls'), name='Connector_urls')
+    path('', include('Connector.urls'), name='Connector_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
